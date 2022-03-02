2969bd55b1577741ed532b4219b3437c944d990b
Sarah Vu <sarahvu@microsoft.com>
2022-03-01 17:25:22 -0800

    add sample app

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StartupLogInvestigationApp;
+using System.Reflection;
+using System;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace StartupLogInvestigationApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Environment.CurrentDirectory)
+               .AddJsonFile("secrets.json")
+               .AddUserSecrets(Assembly.GetExecutingAssembly())
+               .AddEnvironmentVariables()
+               .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
+        }
+    }
+}
